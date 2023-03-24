19ce6cbb6b759fa8576ed9adb6df6a0cba57cb97
stanley <smaraba@neslotech.co.za>
2023-03-24 15:53:00 +0200

    updating controller and service packages

---
+++ src/main/java/com/budgetapp/services/UserRepository.java
@@ -0,0 +1,7 @@
+package com.budgetapp.services;
+
+import com.budgetapp.entities.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface UserRepository extends JpaRepository<User, Long> {
+}
