2c3994e1105e6053d4f85a3b1a9f9f8a105c8727
stanley <smaraba@neslotech.co.za>
2023-03-27 11:06:06 +0200

    TRAIN-174: Refactoring controller classes

--- src/main/java/com/budgetapp/services/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package com.budgetapp.services;
-
-import com.budgetapp.entities.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, Long> {
-}
